8cacf6b9149a57c70692128da50f0b8bb252a288
Will <avatec2312@gmail.com>
2022-12-01 15:17:57 -0500

    changed alert to console.log

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Webpack working properly.");
+function component() {
+    const element = document.createElement('div');
+ 
+   // Lodash, currently included via a script, is required for this line to work
+   // Lodash, now imported by this script
+    element.textContent = "Working ...maybe?"
+ 
+    return element;
+  }
+ 
+document.body.appendChild(component());
+
+alert("hi.")
