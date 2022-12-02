8c0a092d93961cfa4639bbf23c56e6eb704db3f7
Will <avatec2312@gmail.com>
2022-12-01 19:54:09 -0500

    Separate code in modules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-function component() {
-    const element = document.createElement('div');
- 
-   // Lodash, currently included via a script, is required for this line to work
-   // Lodash, now imported by this script
-    element.textContent = "Working ...maybe?"
- 
-    return element;
-  }
- 
-document.body.appendChild(component());
+import Initialize from "./website.js";
 
-alert("hi.")
+Initialize();
---
+++ src/website.js
@@ -0,0 +1,9 @@
+
+
+export default function Initialize (){
+    createHeader();
+    createPresentation();
+    createFooter();
+
+    return
+}
