5a6c0c6333edf61f090b3f8ce9d22598fbbd73d9
Will <avatec2312@gmail.com>
2022-12-03 03:59:27 -0500

    erase variable container, bc never used

--- src/js-modules/main-content.js
+++ src/js-modules/main-content.js
@@ -5,7 +5,6 @@ export default function createPresentation (){
     let text_container = createHelper("div","text-container",section);
     let marca = createHelper("p","marca",text_container);
     let p = createHelper("p",false,text_container);
-    let logo = marca.textContent = "Gabriela";
-    let subtitle = p.textContent = "Taberna & Restaurant";
-    return
+    marca.textContent = "Gabriela";
+    p.textContent = "Taberna & Restaurant";
 }
