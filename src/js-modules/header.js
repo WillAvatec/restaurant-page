25f68422ee5a1c007512e1fa4eae6ee61db0e3e4
Will <avatec2312@gmail.com>
2022-12-03 15:31:46 -0500

    modified gitignore file

--- src/js-modules/header.js
+++ src/js-modules/header.js
@@ -3,11 +3,6 @@ import { createHelper } from "./website";
 export default function createHeader(){
     let section = createHelper("section","head",content);
     let nav = createHelper("nav",false,section);
-    for (let i=0;i<5;i++){
-        let li = createHelper("li",false,nav);
-        let a = createHelper("a",false,li);
-        a.textContent = "link"; 
-    }
-
+    
     return section
 }
