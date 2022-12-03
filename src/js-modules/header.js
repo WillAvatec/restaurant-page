f68f89e9abd45ba59464452aa65ab1124f23246f
Will <avatec2312@gmail.com>
2022-12-03 16:50:44 -0500

    Render links on page

--- src/js-modules/header.js
+++ src/js-modules/header.js
@@ -3,6 +3,11 @@ import { createHelper } from "./website";
 export default function createHeader(){
     let section = createHelper("section","head",content);
     let nav = createHelper("nav",false,section);
-    
+    let homeLink = createHelper("a","home",nav);
+    homeLink.textContent = 'Home';
+    let menuLink = createHelper("a","menu",nav);
+    menuLink.textContent = 'Menu';
+    let contactLink = createHelper("a","contact",nav);
+    contactLink.textContent = 'Contact Us';
     return section
 }
