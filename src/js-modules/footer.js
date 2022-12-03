fba399bedbf10e51dd6f10e5b31ed8ae7fc5da6c
Will <avatec2312@gmail.com>
2022-12-03 03:05:53 -0500

    set href attr to github

--- src/js-modules/footer.js
+++ src/js-modules/footer.js
@@ -4,7 +4,7 @@ export default function createFooter (){
     let section = createHelper("section","footer",content);
     let footer_cont = createHelper("div","footer-container",section);
     let p = createHelper("p",false,footer_cont);
-    p.innerHTML = 'Developed by <a href="#">avatec2312</a>.';
+    p.innerHTML = 'Developed by <a href="https://github.com/WillAvatec/restaurant-page">avatec2312</a>.';
     
     return
 }
