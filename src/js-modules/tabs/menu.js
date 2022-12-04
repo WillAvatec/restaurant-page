9b78d200c0f32423f01885ed0e74833392ab6e7b
Will <avatec2312@gmail.com>
2022-12-04 00:57:30 -0500

    render menu image

---
+++ src/js-modules/tabs/menu.js
@@ -0,0 +1,10 @@
+import Menu from '../../assets/menu.jpg';
+
+export default function (){
+    let section = document.querySelector('.presentation');
+    const myMenu = new Image();
+    myMenu.src = Menu;
+    section.appendChild(myMenu);
+
+    return
+}
